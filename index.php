5b0670c0a70c8a35964be17cdabbf926f8b555bb
kumarajeet024 <kumarajeet024@gmail.com>
2018-09-18 17:43:43 +0530

    php deploy

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>OSSR&DC</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="menu.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+   
+</head>
+<body>
+  
+
+        <header>
+            <h2 id="oss">OSSR&DC</h2>
+          <div class="header1">
+            <div class="hamburger">
+              <button class="button hamburger__button js-menu__toggle">
+                <span class="hamburger__label">Open menu</span>
+              </button>
+            </div>
+
+            <nav class="menu">
+              <ul class="list menu__list">
+                <li class="menu__group">
+                  <a href="#0" class="link menu__link">Home</a>
+                </li>
+                <li class="menu__group">
+                  <a href="#0" class="link menu__link">About me</a>
+                </li>
+                <li class="menu__group">
+                  <a href="#0" class="link menu__link">Services</a>
+                </li>
+                <li class="menu__group">
+                  <a href="#0" class="link menu__link">Portfolio</a>
+                </li>
+                <li class="menu__group">
+                  <a href="#0" class="link menu__link">Blog</a>
+                </li>
+                <li class="menu__group">
+                  <a href="#0" class="link menu__link">Contacts</a>
+                </li>
+              </ul>
+            </nav>
+            </div>
+            
+          </header>
+          <script src="menu.js"></script>
+
+    <div class="about_us">
+        <h2>ABOUT US</h2>
+        <h4>LOREM IPSUM DOLOR SIT AMET, CONSECTETUR</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Porro dolor, hic tempore omnis! Et, quod! Repudiandae itaque necessitatibus,
+           facilis optio debitis incidunt, veniam placeat fuga eos molestiae culpa rem eveniet.
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+           Porro dolor, hic tempore omnis! Et, quod! Repudiandae itaque necessitatibus,
+            facilis optio debitis incidunt, veniam placeat fuga eos molestiae culpa rem eveniet.</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+              Porro dolor, hic tempore omnis! Et, quod! Repudiandae itaque necessitatibus,
+               facilis optio debitis incidunt, veniam placeat fuga eos molestiae culpa rem eveniet.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+               Porro dolor, hic tempore omnis! Et, quod! Repudiandae itaque necessitatibus,
+                facilis optio debitis incidunt, veniam placeat fuga eos molestiae culpa rem eveniet.</p>
+               
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                  Porro dolor, hic tempore omnis! Et, quod! Repudiandae itaque necessitatibus,
+                   facilis optio debitis incidunt, veniam placeat fuga eos molestiae culpa rem eveniet.
+                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                   Porro dolor, hic tempore omnis! Et, quod! Repudiandae itaque necessitatibus,
+                    facilis optio debitis incidunt, veniam placeat fuga eos molestiae culpa rem eveniet.</p>
+
+    </div>
+<!--slideshow for technology we use-->
+
+    <div class="technology">
+
+      </div>
+
+    
+</body>
+</html>
